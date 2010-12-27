291fdac59a7250ada231ca72664f98e1bfc9113d
Bryan Koroleski <korol1bp@cmich.edu>
2010-12-27 18:22:19 -0500

    Overrode setNilValueForKey so the raise attribute gets set to 0.0 instead of nil.

--- Classes/Person.m
+++ Classes/Person.m
@@ -23,6 +23,14 @@
     return self;
 }
 
+- (void)setNilValueForKey:(NSString *)key {
+    if([key isEqual:@"expectedRaise"]) {
+        [self setExpectedRaise:0.0];
+    } else {
+        [super setNilValueForKey:key];
+    }
+}
+
 - (void)dealloc {
     [personName release];
     [super dealloc];
